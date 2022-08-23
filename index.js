832030d4392e54d784b3ad7da9af1392fe40c185
SurenaBot <104827062+paht-test-91@users.noreply.github.com>
2022-08-24 03:55:12 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const client = require('./lib/client')
 const { DATABASE } = require('./config')
 const start = async () => {
-  console.log('HyNO-MD')
+  console.log('wa-bot')
   await DATABASE.sync()
   console.log('DB syncing')
   await client.connect()
